f84a633fd050cb189de92dfd75535a8c074d0d7b
oliversimonics <oliversimonics@gmail.com>
2026-09-15 17:20:18 +0200

    feat: last bit of first class

--- gyak1/week01/src/main/java/week01/Main.java
+++ gyak1/week01/src/main/java/week01/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         var e = new Manager( "Jhon", 10000);
         System.out.println(e);
+        Company con = new Company();
+        System.out.println(con);
     }
 }
